2a12b0442877b7fed8230a692a15c45ad097f455
menjivarj <jonathan.menjivar@bruins.belmont.edu>
2026-04-08 14:52:00 -0700

    Player taking damage
    Player now takes damage when colliding with an enemy, then kills the enemy

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -5,6 +5,8 @@ public class PlayerBehaviour : MonoBehaviour
     private Rigidbody2D rb;
     private Animator walkAnimation;
 
+    public int health;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,5 +19,14 @@ public class PlayerBehaviour : MonoBehaviour
     {
         rb.linearVelocity += new Vector2(Time.deltaTime, 0.0f);
         walkAnimation.speed = rb.linearVelocityX;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            health--;
+            Destroy(collision.gameObject);
+        }
     }
 }
