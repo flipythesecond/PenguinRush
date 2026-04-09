f753098c7b6a247b367461f4bb8ca91425b4eca6
menjivarj <jonathan.menjivar@bruins.belmont.edu>
2026-04-09 13:00:36 -0700

    Spacebar Fix
    Fixed spacebar code

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -34,11 +34,7 @@ public class PlayerBehaviour : MonoBehaviour
         {
             jumpBuffer = true;
         }
-        if (Keyboard.current.space.wasPressedThisFrame)
-        {
-            jumpBuffer = true;
-        }
-
+        
     }
     void FixedUpdate()
     {
