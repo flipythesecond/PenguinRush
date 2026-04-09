b7cf6941239bd1585779734f5775425bbf2dfa75
menjivarj <jonathan.menjivar@bruins.belmont.edu>
2026-04-09 12:01:24 -0700

    Player Enemy and Camera Fixes and Randomized Behaviour
    Fixed snowman throwing snowballs
    Fixed fish animation to play on loop
    Fixed walrus animation to play on jump only
    Added randomized variables for jumping and throwing projectiles
    Main camera now moves relative to the player depending on an offset
    Player now makes sound when taking damage and gaining health
    Player now only accelerates when standing on terrain

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -11,22 +11,35 @@ public class PlayerBehaviour : MonoBehaviour
 
     private bool jumpBuffer;
 
+    public AudioClip damageSound;
+    public AudioClip healthSound;
+    private AudioSource audioSource;
+
+    public float cameraOffsetX;
+    public float cameraOffsetY;
+    private Vector2 cameraVelocity;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         walkAnimation = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity += new Vector2(Time.deltaTime, 0.0f);
-        walkAnimation.speed = rb.linearVelocityX;
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             jumpBuffer = true;
         }
+
+    }
+    void FixedUpdate()
+    {
+        Vector2 newCameraPos = Vector2.SmoothDamp(Camera.main.transform.position, transform.position, ref cameraVelocity, Time.deltaTime);
+        Camera.main.transform.SetPositionAndRotation(new Vector3(newCameraPos.x + cameraOffsetX, newCameraPos.y + cameraOffsetY, -10), Quaternion.identity);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -34,6 +47,14 @@ public class PlayerBehaviour : MonoBehaviour
         if (collision.gameObject.CompareTag("Enemy"))
         {
             health--;
+            audioSource.PlayOneShot(damageSound);
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Fish"))
+        {
+            health++;
+            audioSource.PlayOneShot(healthSound);
             Destroy(collision.gameObject);
         }
     }
@@ -44,6 +65,12 @@ public class PlayerBehaviour : MonoBehaviour
         {
             rb.linearVelocityY = jumpHeight;
             jumpBuffer = false;
+        }
+
+        if (collision.gameObject.CompareTag("Terrain"))
+        {
+            rb.linearVelocity += new Vector2(Time.fixedDeltaTime, 0.0f);
+            walkAnimation.speed = rb.linearVelocityX;
         }
     }
 }
