d090f269ad92d41825ceef2fed4254e761e8f48e
Flipy <bekanvarov7@gmail.com>
2026-04-09 23:32:55 -0500

    Added WebGL build, Bug Fixes, Extra Assets
    Added WebGL build, Fixed background video not playing in WebGL version, Bug Fixes, Extra Assets

---
+++ Assets/Scripts/VideoSetFix.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Video;
+public class VideoSetFix : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        videoPlayer.source = VideoSource.Url;
+
+        string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, "mountain_loop.mp4");
+        videoPlayer.url = videoPath;
+
+        videoPlayer.isLooping = true;
+        videoPlayer.playOnAwake = false;
+
+        videoPlayer.SetDirectAudioMute(0, true);
+
+        videoPlayer.Play();
+    }
+}
